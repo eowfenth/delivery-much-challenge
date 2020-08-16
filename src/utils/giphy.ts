6e28a712f92c0e5b50c4a3ebd267bbb21655c223
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-16 20:10:18 -0300

    feat: add giphy module to handle giphy requests

---
+++ src/utils/giphy.ts
@@ -0,0 +1,40 @@
+import giphyClient from 'giphy-api';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export interface GiphyResponse {
+    status: number;
+    data: GiphySuccessResponse | GiphyErrorResponse;
+}
+
+export interface GiphySuccessResponse {
+    url: string;
+}
+
+interface GiphyErrorResponse {
+    message: string;
+}
+
+const giphy = giphyClient(process.env.GIPHY_API_KEY);
+
+const search = async (content: string): Promise<GiphyResponse> => {
+    let response;
+    try {
+        const gif = (await giphy.search(content)).data.shift();
+        response = { url: gif?.bitly_url ?? '' };
+    } catch (err) {
+        console.error(JSON.stringify(err));
+        return {
+            status: 503,
+            data: { message: 'Internal Server Error. Serviço indisponível.' },
+        };
+    }
+
+    return {
+        status: 200,
+        data: response,
+    };
+};
+
+export default { search };
