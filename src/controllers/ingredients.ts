24af60126c1a8e5395651667693a0329f32c63ac
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-16 17:55:43 -0300

    fix: response structure for Ingredients

--- src/controllers/ingredients.ts
+++ src/controllers/ingredients.ts
@@ -1,17 +1,31 @@
 import { ParameterizedContext, Next } from 'koa';
 
+interface IngredientsResponse {
+    keywords: string[];
+    recipes: {
+        title: string;
+        ingredients: string[];
+        link: string;
+        gif: string;
+    }[];
+}
 const querystringParser = (querystring: string): string[] => {
     const parsedContent = querystring.replace(/\s*,\s*/g, ',').split(',');
 
     return parsedContent;
 };
 
-const searchFor = async (ctx: ParameterizedContext, next: Next): Promise<void> => {
+const searchFor = async (ctx: ParameterizedContext, next: Next): Promise<IngredientsResponse> => {
     if (!ctx.request.query && !ctx.request.query.i) {
         ctx.body = { status: 400, message: 'Bad Format. You need to pass ingredients' };
     }
 
     const parsedQuerystring = querystringParser(ctx.request.query.i);
+
+    ctx.body = {
+        keywords: parsedQuerystring,
+        recipes: [],
+    };
 
     await next();
 };
